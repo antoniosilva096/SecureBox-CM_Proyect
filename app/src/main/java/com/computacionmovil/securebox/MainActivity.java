2d02e95684fc07189ccf523eab6d872892be338e
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2024-04-07 20:41:44 +0200

    Finalización de DrawerLayout (menú lateral) y manego de las vistas records, settings, info y exit

--- app/src/main/java/com/computacionmovil/securebox/MainActivity.java
+++ app/src/main/java/com/computacionmovil/securebox/MainActivity.java
@@ -3,12 +3,17 @@ package com.computacionmovil.securebox;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.widget.Toolbar; //Importante importar el mismo toolbar que en el xml
 
+import com.computacionmovil.securebox.Fragmentos.F_Records;
+import com.computacionmovil.securebox.Fragmentos.F_Settings;
 import com.example.securebox.R;
 import com.google.android.material.navigation.NavigationView;
 
@@ -43,10 +48,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         toggle.syncState();
 
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new F_Records()).commit(); //Establecemos el fragmento que muestra los registros como el que se muestra por defecto
+
+            navigationView.setCheckedItem(R.id.Option_All);
+        }
+
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+
+        //Aquí controlaremos los fragmentos
+        //Vamos a asociar cada fragmentocon el evento de pulsar uno de los "botones"(items) del menú lateral
+        //Estó modificará la vista que se presenta ya que solo se visualizará el fragmento seleccionado que se haya vinculado al item
+
+        int id = menuItem.getItemId();
+        if (id == R.id.Option_All){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new F_Records()).commit();
+        }
+
+        if(id == R.id.Option_Settings){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new F_Settings()).commit();
+        }
+
+        if (id == R.id.Option_Exit){
+            Toast.makeText(this, "Logout...", Toast.LENGTH_SHORT).show();
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
