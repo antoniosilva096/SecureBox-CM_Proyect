83c8d90a55a7a1082d6ea4eca02afe195c1af505
antoniosilva096 <36417018+antoniosilva096@users.noreply.github.com>
2024-05-01 21:29:35 +0200

    Creación del botón guardar registro y sincronización con firebase

--- app/src/main/java/com/computacionmovil/securebox/Fragmentos/F_Records.java
+++ app/src/main/java/com/computacionmovil/securebox/Fragmentos/F_Records.java
@@ -1,5 +1,6 @@
 package com.computacionmovil.securebox.Fragmentos;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,14 +9,28 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.computacionmovil.securebox.OptionsRecords.Add_Record;
 import com.example.securebox.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class F_Records extends Fragment {
+
+    FloatingActionButton FAB_AddButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_f__records, container,false); //Lamamos a la vista del fragmento de registros
-        return inflater.inflate(R.layout.fragment_f__records, container, false);
+
+
+        FAB_AddButton = view.findViewById(R.id.FAB_AddButton);
+
+        FAB_AddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), Add_Record.class)); //Vinculamos el evento de hacer click en el botón flotante con invocar a la actividad de agregar registro
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/computacionmovil/securebox/OptionsRecords/Add_Record.java
+++ app/src/main/java/com/computacionmovil/securebox/OptionsRecords/Add_Record.java
@@ -1,8 +1,15 @@
 package com.computacionmovil.securebox.OptionsRecords;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -12,9 +19,43 @@ import com.example.securebox.R;
 
 public class Add_Record extends AppCompatActivity {
 
+    EditText EtTitle, EtAccount, EtUserName, EtPassword, EtUrl, EtNotes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_record);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setTitle("");
+
+        inicializarVariables();
+    }
+
+    private void inicializarVariables(){
+        EtTitle = findViewById(R.id.EtTitle);
+        EtAccount = findViewById(R.id.EtAccount);
+        EtUserName = findViewById(R.id.EtUserName);
+        EtPassword = findViewById(R.id.EtPassword);
+        EtUrl = findViewById(R.id.EtUrl);
+        EtNotes = findViewById(R.id.EtNotes);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_guardar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.save_password){
+            Toast.makeText(this, "Save record", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
